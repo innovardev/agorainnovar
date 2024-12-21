fea3912c4796bc8c8141739331f3efc924035865
Andres Garcia <felipegarcia1806@gmail.com>
2024-12-21 10:02:55 -0500

    correccion api soap

--- src/app/services/saturno.service.ts
+++ src/app/services/saturno.service.ts
@@ -2,40 +2,37 @@ import axios from "axios";
 
 const createSoapRequest = () => `
 <?xml version="1.0" encoding="utf-8"?>
-<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-  <soap12:Body>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
     <ConsultaInventarioShopify xmlns="http://tempuri.org/">
       <Usuario>Shopify</Usuario>
       <Contrasena>Shopify1</Contrasena>
     </ConsultaInventarioShopify>
-  </soap12:Body>
-</soap12:Envelope>
+  </soap:Body>
+</soap:Envelope>
 `;
 
 const callSoapApi = async () => {
+  const url = 'http://181.57.183.198/eCommerce.asmx';
 
-    const url = 'http://181.57.183.198:8092/eCommerce.asmx?op=ConsultaInventarioShopify'
-    try {
-        const headers = {
-            'Content-Type': 'application/soap+xml; charset=utf-8',
-        }
+  try {
+    const headers = {
+      'Content-Type': 'text/xml; charset=utf-8',
+      'SOAPAction': '"http://tempuri.org/ConsultaInventarioShopify"',
+    };
 
-        const response = await axios.post(url, createSoapRequest(), { headers })
-
-        const jsonResponse = response.data
-
-        return jsonResponse
-
-    } catch (error:any) {
-        if (error.response) {
-            console.error('Error en la respuesta:', error.response.data);
-          } else if (error.request) {
-            console.error('Sin respuesta del servidor:', error.request);
-          } else {
-            console.error('Error en la configuración de la solicitud:', error.message);
-          }
+    const response = await axios.post(url, createSoapRequest(), { headers });
+    console.log('Respuesta del servidor:', response.data);
+  } catch (error:any) {
+    if (error.response) {
+      console.error('Error en la respuesta:', error.response.data);
+    } else if (error.request) {
+      console.error('Sin respuesta del servidor:', error.request);
+    } else {
+      console.error('Error en la configuración de la solicitud:', error.message);
     }
+  }
+};
 
-}
 
 export default callSoapApi;
